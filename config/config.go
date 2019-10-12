61dab72552c95edf59221186e3dbac4283a20107
thatguywiththatname <simjenner3@gmail.com>
2019-10-12 19:02:25 +0100

    Move config to own package, general code cleanup, compelte some inline TODOs

---
+++ config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"github.com/getlantern/systray"
+	"io/ioutil"
+	"os"
+)
+
+const configFileDir = "./config-files/"
+const configFileMaster = "../config.txt"
+
+type EApoConfig struct {
+	Name     string
+	Data     []byte
+	MenuItem *systray.MenuItem
+}
+
+// Takes a FileInfo interface. Assuming that file is in configFileDir, load the contents of that file into an
+// EApoConfig struct and return it.
+func configStructFromFile(file os.FileInfo) (EApoConfig, error) {
+	fileName := file.Name()
+
+	dat, err := ioutil.ReadFile(configFileDir + fileName)
+	if err != nil {
+		return EApoConfig{}, err
+	}
+
+	return EApoConfig{
+		Name: fileName,
+		Data: dat,
+	}, nil
+}
+
+// Creates a slice of EApoConfigs which contain the data for all config files in configFileDir.
+func CreateConfigSlice() []*EApoConfig {
+	var configSlice []*EApoConfig
+
+	files, err := ioutil.ReadDir(configFileDir)
+	if err != nil {
+		// Can't read config file directory, so can't do anything else.
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			configStruct, err := configStructFromFile(file)
+			if err == nil {
+				configSlice = append(configSlice, &configStruct)
+			}
+		}
+	}
+
+	return configSlice
+}
+
+// Write all items in configSlice to configFileMaster if their associated menu item is checked.
+func WriteConfigToMaster(configSlice []*EApoConfig) {
+	var completeData []byte
+	newline := []byte("\n")
+
+	for _, config := range configSlice {
+		if config.MenuItem.Checked() {
+			// "..." -> https://stackoverflow.com/a/16248257/6396652
+			completeData = append(completeData, newline...)
+			completeData = append(completeData, config.Data...)
+		}
+	}
+
+	err := ioutil.WriteFile(configFileMaster, completeData, 0644)
+	if err != nil {
+		// Can't write to configFileMaster.
+		// TODO: Maybe show an error, or log it somehow?
+		os.Exit(1)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,37 +1,13 @@
 package main
 
-// go build -o E-APO-Config-Switcher.exe github.com/psidex/E-APO-Config-Switcher
-
 import (
-	"fmt"
 	"github.com/getlantern/systray"
+	"github.com/psidex/E-APO-Config-Switcher/config"
 	"github.com/psidex/E-APO-Config-Switcher/icon"
-	"io/ioutil"
 )
 
 func main() {
 	systray.Run(onReady, onExit)
-}
-
-func writeConfigToMaster(configSlice []*EApoConfig) {
-	// Takes a slice of EApoConfigs and writes each checked one to configFileMaster
-	var completeData []byte
-	newline := []byte("\n")
-
-	for _, config := range configSlice {
-		if config.MenuItem.Checked() {
-			// "..." -> https://stackoverflow.com/a/16248257/6396652
-			completeData = append(completeData, newline...)
-			completeData = append(completeData, config.Data...)
-		}
-	}
-
-	err := ioutil.WriteFile(configFileMaster, completeData, 0644)
-	if err != nil {
-		// TODO: Do something more than just this
-		fmt.Println(err)
-		return
-	}
 }
 
 func onReady() {
@@ -39,16 +15,13 @@ func onReady() {
 	systray.SetTitle("E-APO Config Switcher")
 	systray.SetTooltip("E-APO Config Switcher")
 
-	configSlice := CreateConfigSlice()
+	configSlice := config.CreateConfigSlice()
 
-	for _, config := range configSlice {
-		btn := systray.AddMenuItem(config.Name, "tooltip")
-		config.MenuItem = btn
+	for _, configStruct := range configSlice {
+		btn := systray.AddMenuItem(configStruct.Name, "Activate / Deactivate this config")
+		configStruct.MenuItem = btn
 
-		// config is passed to the routine because it's one variable whose value is being changed every loop, so by the
-		// time the routine is run, the value may be different. Btn is a different variable each loop so does not need
-		// to be passed
-		go func(config *EApoConfig) {
+		go func() {
 			for {
 				<-btn.ClickedCh
 				if !btn.Checked() {
@@ -56,9 +29,9 @@ func onReady() {
 				} else {
 					btn.Uncheck()
 				}
-				writeConfigToMaster(configSlice)
+				config.WriteConfigToMaster(configSlice)
 			}
-		}(config)
+		}()
 	}
 
 	QuitBtn := systray.AddMenuItem("Quit", "Quit the whole app")
@@ -71,5 +44,5 @@ func onReady() {
 }
 
 func onExit() {
-	// clean up here
+	// No cleanup needed
 }
