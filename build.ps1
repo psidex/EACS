d334ade706d5eb33c32b3d261ce3ef328f80c529
thatguywiththatname <simjenner3@gmail.com>
2020-04-11 03:59:48 +0100

    Update build script to not compile with debug info

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
-# See https://github.com/getlantern/systray#windows for the manifest file
 # Generate a syso file
+# See https://github.com/getlantern/systray#windows for the manifest file
 rsrc -manifest EACS.manifest -ico icon\icon.ico -o EACS.syso
 # Build - automatically uses *.syso files
-go build -ldflags -H=windowsgui -v .
+# "-s -w" removes debugging stuff
+go build -ldflags "-s -w -H=windowsgui" -v .
