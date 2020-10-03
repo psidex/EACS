c5e53b6be0d031892ec4a9477c0e957a5691ced9
thatguywiththatname <simjenner3@gmail.com>
2020-10-03 03:15:06 +0100

    Write to config.txt when uncheck all button pressed

--- internal/buttons/uncheckall.go
+++ internal/buttons/uncheckall.go
@@ -4,6 +4,7 @@ import (
 	"github.com/getlantern/systray"
 	"github.com/psidex/EACS/internal/appdata"
 	"github.com/psidex/EACS/internal/icon"
+	"github.com/psidex/EACS/internal/userconfig"
 	"github.com/psidex/EACS/internal/util"
 )
 
@@ -18,9 +19,14 @@ func UncheckAllHandler(uncheckAllBtn *systray.MenuItem, dc *appdata.DataControll
 			btn.Uncheck()
 		}
 
+		err := userconfig.WriteIncludesToMainConfig([]string{})
+		if err != nil {
+			util.FatalError(err)
+		}
+
 		systray.SetIcon(icon.DataInactive)
 
-		err := dc.Save()
+		err = dc.Save()
 		if err != nil {
 			util.FatalError(err)
 		}
